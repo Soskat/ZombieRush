a189b77dff44fbe074a073894d2ac39eaa70b9f1
Soskat <soskat@outlook.com>
2016-10-23 15:50:31 +0200

    move method was refactored

--- Player.py
+++ Player.py
@@ -7,18 +7,18 @@ import zrcommon
 """ Class that represents Player """
 class Player:
 	""" Constructor """
-	def __init__(self, gameDisplay, displaySize, color, levelObst):
+	def __init__(self, gameDisplay, displaySize, color, level):
 		self.__screen = gameDisplay				# game display
 		self.__color = color					# Player's color
-		self.__lvlObst = levelObst				# level obstacles info
+		self.__level = level					# level handler
 		self.__vOr = [(-8,10), (0,-10), (8,10)]	# origin vertices
 		self.__posX = 400	# X position
 		self.__posY = 300	# Y position
 		self.__heading = 0	# heading in degrees from UP direction to right
-		self.__radius = 10	# Player's radius used in collision detection
-		self.__maxX = displaySize[0] - self.__radius	# game world border
-		self.__maxY = displaySize[1] - self.__radius	# game world border
-		self.__minX = self.__minY = self.__radius		# game world borders
+		self.radius = 10	# Player's radius used in collision detection
+		self.__maxX = displaySize[0] - self.radius	# game world border
+		self.__maxY = displaySize[1] - self.radius	# game world border
+		self.__minX = self.__minY = self.radius		# game world borders
 
 
 	""" Draws Player """
@@ -46,24 +46,6 @@ class Player:
 		if y < self.__minY:	y = self.__minY
 		elif y > self.__maxY: y = self.__maxY
 		# check collisions with obstacles:
-		keyX = int(x / 100)
-		keyY = int(y / 100)
-		for kx in range(keyX - 1, keyX + 2):
-			if kx in self.__lvlObst:
-				for ky in range (keyY - 1, keyY + 2):
-					if ky in self.__lvlObst[kx]:
-						for obst in self.__lvlObst[kx][ky]:
-							condition, info = obst.if_collide((x, y, self.__radius))
-							# in case of collision adjust Player's position to avoid it:
-							if condition:
-								x, y = self.__adjust_position((x,y), info)
+		x, y = self.__level.avoid_collision_with_obstacles((x,y,self.radius))
 		# move Player:
 		self.__posX, self.__posY = x, y
-
-
-	""" Adjusts position to avoid collision """
-	def __adjust_position(self, newPos, collInfo):
-		dx, dy, d, D = collInfo
-		x = dx * (D - d) / d
-		y = dy * (D - d) / d
-		return newPos[0] - x, newPos[1] - y
