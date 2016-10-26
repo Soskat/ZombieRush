e60c4c25b7e92831a459b2b8d63bdf62f88b0772
Soskat <soskat@outlook.com>
2016-10-26 19:04:13 +0200

    Basics of steering behaviours were implemented

--- Zombie.py
+++ Zombie.py
@@ -2,6 +2,7 @@
 #-*- coding: utf-8 -*-
 
 import pygame
+from zrcommon import Vector2D
 from SteeringBehaviours import SteeringBehaviours
 
 
@@ -9,44 +10,55 @@ from SteeringBehaviours import SteeringBehaviours
 class Zombie:
     """ Constructor """
     def __init__(self, screen, level, player,
-                       ID, position, radius, mass, max_velocity, max_force, max_turn_rate,
+                       ID, pos, radius, mass, max_velocity, max_force, max_turn_rate,
                        color):
-        self.__screen = screen      # game display handler
-        self.__level = level        # Level handler
-        self.__player = player      # Player handler
-        self.__color = color        # zombie's color
-        self.ID = ID                # ID number
-        self.posX = position[0]     # x posotion
-        self.posY = position[1]     # y position
-        self.radius = radius        # radius
-        #self.__heading = [0.0, 0.0]             # heading vector
-        #self.__v_side = [0.0, 0.0]              # vector perpendicular to heading vector    <-------------------------------------------------- DO IT RIGHT
-        #self.__velocity = 0.0                   # current velocity
-        self.__mass = mass                      # mass
-        self.__max_velocity = max_velocity      # maximum velocity at which bot can travel
+        self.__screen = screen              # game display handler
+        self.__level = level                # Level handler
+        self.__player = player              # Player handler
+        self.__color = color                # zombie's color
+        self.ID = ID                        # ID number
+        self.pos = Vector2D(pos[0], pos[1]) # position
+        self.radius = radius                # radius
+        """ steering behaviours """
+        self.velocity = Vector2D()              # velocity vector
+        self.__heading = Vector2D()             # heading vector
+        self.__v_side = self.__heading.perp()   # vector perpendicular to heading vector
+        self.__mass = 1 / mass                  # mass used in calculations
+        self.max_velocity = max_velocity        # maximum velocity at which bot can travel
         self.__max_force = max_force            # maximum force that bot can produce to power itself
         self.__max_turn_rate = max_turn_rate    # maximum rate at which bot can rotate
-        """ steering behaviours """
-        self.__steering = SteeringBehaviours(self)
-        self.__heading = [0.0, 0.0]             # heading vector
-        self.__v_side = [0.0, 0.0]              # vector perpendicular to heading vector    <-------------------------------------------------- DO IT RIGHT
-        self.__velocity = 0.0                   # current velocity
-        self.__st_force = [0.0, 0.0]
+        self.__steering = SteeringBehaviours(self, self.__max_force)
 
 
     """ Move zombie bot """
     def move(self):
-        self.__st_force = self.__steering.seek(self.__player.get_position())
-        acceleration = [self.__st_force[i] / self.__mass for i in range(len(self.__st_force)) ]
+        TIME_ELAPSED = 0.3      #<====================================================================================== ??
+        # calculate vehicle position based on steering forces:
+        steering_force = self.__steering.calculate()
+        print("steering_force after truncate")
+        steering_force.print_v()
+        # Acceleration = Force / Mass
+        acceleration = steering_force.mult(self.__mass)
+        print("acceleration")
+        acceleration.print_v()
+        # update velocity:
+        self.velocity = self.velocity.add(acceleration.mult(TIME_ELAPSED)).trunc(self.max_velocity)
+        # update position:
+        self.pos = self.pos.add(self.velocity.mult(TIME_ELAPSED))
+        # update heading if zombie has a velocity greater than a very small value:
+        if self.velocity.magn() > 0.0000001:
+            self.__heading = self.velocity.norm()
+            self.__v_side = self.__heading.perp()
+
 
     """ Draws zombie bot """
     def draw(self):
         pygame.draw.circle(self.__screen, self.__color,
-                           (self.posX, self.posY),
+                           (int(self.pos.x), int(self.pos.y)),
                            self.radius,
                            2)
 
 
-    """ Get zombie bot position """
-    def get_position(self):
-        return [self.posX, self.posY]
+    """ Get player position """
+    def get_target(self):
+        return self.__player.get_pos()
