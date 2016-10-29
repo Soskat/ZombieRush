701e553e992c5d14e2d485d04db0d7bf5fb83cf9
Soskat <soskat@outlook.com>
2016-10-29 15:29:24 +0200

    File with constants used in game

---
+++ constants.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+# colors
+YELLOW = (255, 255, 0)
+BLACK = (0, 0, 0)
+GREY = (120, 120, 120)
+GREEN = (0, 255, 0)
+# debug colors:
+APPLE_GREEN = (0, 0, 255)
+RED = (255, 0, 0)
+ORANGE = (255, 149, 0)
+
+
+# other constants
+panic_distance = 100
