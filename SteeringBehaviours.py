74aa0a2607f34bb13274a4bf2b5fc7597838a768
Soskat <soskat@outlook.com>
2016-10-29 15:31:31 +0200

    Flee behaviour update - stage I

--- SteeringBehaviours.py
+++ SteeringBehaviours.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+import constants as c
+
+
+
 import zrcommon as zrc
 from zrcommon import Vector2D
 
@@ -25,7 +29,20 @@ class SteeringBehaviours:
 
 
     """ Flee """
-    def flee(self, target):
+    def flee(self, target, panic_distance):
+        # flee only when the target is inside panic_distance:
+        target = self.__veh.get_target()
+        #target.print_v("target pos")
+        #self.__veh.pos.print_v("zombie pos")
+        if not zrc.check_collision(
+                                    (self.__veh.pos.x, self.__veh.pos.x, self.__veh.radius),
+                                    (target.x, target.y, panic_distance)
+                                  ):
+            self.debug_color = c.APPLE_GREEN
+            return Vector2D()
+
+        print("panic!")
+        self.debug_color = c.RED
         desired_velocity = zrc.sub_vectors(self.__veh.pos, self.__veh.get_target())
         desired_velocity.norm().mult(self.__veh.max_velocity)
         return zrc.sub_vectors(desired_velocity, self.__veh.velocity)
@@ -33,10 +50,12 @@ class SteeringBehaviours:
 
     """ Caculate all steeering forces that worked on vehicle """
     def calculate(self):
+        panic_distance = 100
         """ The most basic system is used - change this later! """              #< ========================= BUKA
         steering_force = Vector2D()
         # sum all steering forces together:
         #if self.seek_on: steering_force.add(self.seek(self.__veh.get_target()))
-        if self.flee_on: steering_force.add(self.flee(self.__veh.get_target()))
+        if self.flee_on:
+            steering_force.add(self.flee(self.__veh.get_target(), panic_distance))
         steering_force.trunc(self.__max_force)
         return steering_force
