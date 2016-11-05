2befbce0877858497b909f4c4de251385809a6c5
Soskat <soskat@outlook.com>
2016-11-05 10:46:16 +0100

    Constants were updated

--- constants.py
+++ constants.py
@@ -12,6 +12,11 @@ BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 ORANGE = (255, 149, 0)
 DARKYELLOW = (232, 178, 0)
+# FSM zombie colors
+Z_ATTACK = (166, 0, 0)
+Z_FLEE = (0, 255, 238)
+Z_HIDDEN = (0, 130, 0)
+Z_TAKE_RISK = (0, 180, 238)
 ##########################
 ### game constants
 ##########################
@@ -24,7 +29,7 @@ player_max_force = 5.0
 player_max_turn_rate = 0.2
 # zombie related constants
 zombie_amount = 100
-current_zombie_amount = 10
+current_zombie_amount = 1
 zombie_color = GREEN
 zombie_radius = 8
 zombie_mass = 2
@@ -38,6 +43,9 @@ deceleration_tweaker = 0.3
 decelerate_SLOW = 3.0
 decelerate_NORMAL = 2.0
 decelerate_FAST = 1.0
+wandern_radius = 50
+wandern_distance = 50
+wandern_jitter = 80.0
 # finite state mashine states:
 state_IDLE = 0
 state_FLEE = 1
