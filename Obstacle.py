929b1cb604e8fd35ffe95b22079d84afa43b922d
Soskat <soskat@outlook.com>
2016-11-06 15:57:28 +0100

    center field was refactored to use Vector2D class

--- Obstacle.py
+++ Obstacle.py
@@ -3,6 +3,7 @@
 
 import pygame
 import zrcommon
+from Vector2D import Vector2D
 
 
 
@@ -11,22 +12,27 @@ class Obstacle:
     """ Constructor """
     def __init__(self, game_display, center, radius, color):
         self.__screen = game_display
-        self.center = center
+        self.center = Vector2D(center[0], center[1])
         self.radius = radius
         self.__color = color
 
 
     """ Draws Obstacle """
     def draw(self):
-        pygame.draw.circle(self.__screen, self.__color, self.center, self.radius, 1)
+        pygame.draw.circle(self.__screen,
+                           self.__color,
+                           (self.center.x, self.center.y),
+                           self.radius,
+                           1
+                          )
 
 
     """ Checks if Obstacle collides with given object """
     def is_collided(self, ob):
-        return zrcommon.check_collision((self.center[0], self.center[1], self.radius), ob)
+        return zrcommon.check_collision((self.center.x, self.center.y, self.radius), ob)
 
 
     """ Checks if Obstacle collides with given object.
         If yes, recalculate position of given object to avoid collision """
     def avoid_collision(self, ob):
-        return zrcommon.avoid_collision((self.center[0], self.center[1], self.radius), ob)
+        return zrcommon.avoid_collision((self.center.x, self.center.y, self.radius), ob)
