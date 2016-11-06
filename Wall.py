a3456e1563b65b413a78e7503eaa8ec45f5708fe
Soskat <soskat@outlook.com>
2016-11-06 20:30:53 +0100

    New methods were added

--- Wall.py
+++ Wall.py
@@ -2,6 +2,7 @@
 #-*- coding: utf-8 -*-
 
 import zrcommon as zrc
+from Vector2D import Vector2D
 
 
 
@@ -13,7 +14,23 @@ class Wall:
         self.__b = b
         self.__n = self.calculate_normal()
 
+
     """ Calculates normal vector """
     def calculate_normal(self):
-        temp = zrc.sub_vectors(a, b).norm()
+        temp = zrc.sub_vectors(self.__a, self.__b).norm()
         return Vector2D(-temp.y, temp.x)
+
+
+    """ Returns the start point of the wall """
+    def start_point(self):
+        return self.__a
+
+
+    """ Returns the end point of the wall """
+    def end_point(self):
+        return self.__b
+
+
+    """ Returns the normal vector of the wall """
+    def normal_v(self):
+        return self.__n
