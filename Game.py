fd243d56ddea7936218c63f7567e73e82ad41787
Soskat <soskat@outlook.com>
2016-11-21 21:50:46 +0100

    Game menu was added

--- Game.py
+++ Game.py
@@ -32,6 +32,7 @@ ray_cooldown = fps
 ################################################################################
 move_FORWARD = move_BACKWARD = move_LEFT = move_RIGHT = False
 play_again = False
+play_game = True
 ################################################################################
 
 """ Calculates game world walls """ #-------------------------------------------------- is this used for real?
@@ -49,7 +50,7 @@ def calculate_walls():
     return walls
 
 
-""" Draw GUI """
+""" Draws GUI """
 def draw_gui(player_hp, score, wave):
     # PLAYER HEALTH ============================================================
     # draw player HP counter:
@@ -81,20 +82,58 @@ def draw_gui(player_hp, score, wave):
     game_display.blit(sc_label, sc_label_pos)
 
 
-""" Game manager """
-def game_manager():
-    global play_again
+""" Shows game menu """
+def show_menu():
+    global play_again, play_game
     while True:
-        game_loop()
-        if not play_again: return
-        else: play_again = False
+        # check game input: ======================================
+        for event in pygame.event.get():
+            # quit game window:
+            if event.type == pygame.QUIT:
+                play_game = False
+                return
+            # manage keyboard input:
+            if event.type == pygame.KEYDOWN:
+                # quit game:
+                if event.key == pygame.K_q:
+                    play_game = False
+                    return
+                # restart game:
+                if event.key == pygame.K_r:
+                    play_game = False
+                    play_again = True
+                    return
+                # resume game:
+                if event.key == pygame.K_ESCAPE:
+                    return
+        # print menu info: =======================================
+        # resume game:
+        resume_label = game_font.render("Resume game: [ESC]", True, c.WHITE)
+        resume_label_pos = resume_label.get_rect()
+        resume_label_pos.centerx = game_display.get_rect().centerx
+        resume_label_pos.centery = game_display.get_rect().centery - 50
+        game_display.blit(resume_label, resume_label_pos)
+        # restart game:
+        restart_label = game_font.render("Restart game: [R]", True, c.WHITE)
+        restart_label_pos = restart_label.get_rect()
+        restart_label_pos.centerx = game_display.get_rect().centerx
+        restart_label_pos.centery = game_display.get_rect().centery
+        game_display.blit(restart_label, restart_label_pos)
+        # quit game:
+        quit_label = game_font.render("Quit game: [Q]", True, c.WHITE)
+        quit_label_pos = quit_label.get_rect()
+        quit_label_pos.centerx = game_display.get_rect().centerx
+        quit_label_pos.centery = game_display.get_rect().centery + 50
+        game_display.blit(quit_label, quit_label_pos)
+        # update game window: =============================
+        pygame.display.update()
+        clock.tick(fps)
 
 
 """ Main game loop """
 def game_loop():
-    global move_FORWARD, move_BACKWARD, move_LEFT, move_RIGHT, play_again
+    global move_FORWARD, move_BACKWARD, move_LEFT, move_RIGHT, play_again, play_game
 
-    play_game = True
     # draw death ray flags and timer:
     can_use_ray = c.ray_READY
     ray_timer = 0
@@ -109,6 +148,7 @@ def game_loop():
     zombie_pool = ZombiePool(game_display, display_size, player, level, calculate_walls(), menu_font)
 
     print("================ Start Zombie Rush ================")
+    print(play_game)
     while play_game:
         # check game input:
         for event in pygame.event.get():
@@ -129,13 +169,9 @@ def game_loop():
                 # move backward:
                 if event.key in [pygame.K_DOWN, pygame.K_s]:
                     move_BACKWARD = True
-                """ QUICK QUIT """
-                if event.key == pygame.K_SPACE:
-                    play_game = False
-                """ PLAY AGAIN """
-                if event.key == pygame.K_r:
-                    play_game = False
-                    play_again = True
+                # show game menu:
+                if event.key == pygame.K_ESCAPE:
+                    show_menu()
                 """ SWITCH DEBUG MODE """
                 if event.key == pygame.K_u:
                     if debug_flag:
@@ -222,6 +258,17 @@ def game_loop():
 
 
         print("================================================")
+
+
+""" Game manager """
+def game_manager():
+    global play_again, play_game
+    while True:
+        game_loop()
+        if not play_again: return
+        else:
+            play_again = False
+            play_game = True
 ################################################################################
 game_manager()
 
