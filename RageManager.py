a2f685dacc8332260ff305b2d29bd545d3426c87
Soskat <soskat@outlook.com>
2016-11-22 19:33:06 +0100

    RageManager class was added to project

---
+++ RageManager.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+
+import constants as c
+
+
+
+""" Class that manages variables and conditions relatedo to Zombie's rage mode """
+class RageManager:
+    """ Constructor """
+    def __init__(self, display_size):
+        self.rage_circle = c.rage_neighbour_distance    # rage area
+        self.rage_team = 4                              # number of zombies that invoking their rage
+        self.gw_space = {}                              # game world space partitioning dictionary
+        for kx in range(0, int(display_size[0] / 100)):
+            self.gw_space[kx] = {}
+            for ky in range(0, int(display_size[1] / 100)):
+                self.gw_space[kx][ky] = []
